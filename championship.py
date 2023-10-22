95d6dda49298b842d1164a0d8c60dc66a828f158
SayfullayevBekzod <sayfullayevbekzod465@gmail.com>
2023-10-22 07:30:19 +0500

    T4

--- championship.py
+++ championship.py
@@ -1,11 +1,26 @@
 from haydovchi import Haydovchi
 from gp import GP
+from time1 import Time, TugashVaqti
 
 class Championship:
 
     def __init__(self) -> None:
         self.driver_list: list[Haydovchi] = []
+        self.driver_time_list: list[Time] = []
         self.gp_list: list[GP] = []
+
+    def add_gpdriver(self, gp: GP, driver: Haydovchi, tugash_vaqti: TugashVaqti):
+        for _gp in self.gp_list:
+            if gp.name == _gp.name:
+                driver.set_end_time(tugash_vaqti)
+                _gp.collect_driver(driver)
+
+    def set_time(self, gp: GP, driver: Haydovchi, tugash_vaqti: TugashVaqti):
+        driver.set_gp(gp)
+        time = Time(gp, driver, tugash_vaqti)
+        self.add_gpdriver(gp, driver, tugash_vaqti)
+        self.driver_time_list.append(time)
+        return time
 
     def defineGrandPrix(self, name):
         gp = GP(name)
--- main.py
+++ main.py
@@ -1,28 +1,34 @@
 from championship import Championship
-from haydovchi import Haydovchi
 from gp import GP
-from time1 import Time
+from haydovchi import Haydovchi
+from time1 import TugashVaqti
 
 chempionat = Championship()
-time = Time()
-time.setTime(1, 30, 45, 500)
-print(time)  # Soat:minut:soniya.millisikund formatida vaqtni chiqaradi
+
+chempionat.defineGrandPrix("katta halqa")
+gp = GP("katta halqa")
+gp1 = GP("tekis yolak")
+driver1 = Haydovchi('ali')
+driver2 = Haydovchi('vali')
+
+tugash_vaqti1 = TugashVaqti(11, 35, 34, 123)
+tugash_vaqti2 = TugashVaqti(11, 35, 34, 122)
+tugash_vaqti3 = TugashVaqti(11, 35, 34, 120)
 
 
-chempionat1 = Haydovchi("Ali",120)
-chempionat2 = Haydovchi("Alijon", 130)
-chempionat3 = Haydovchi("Vali", 150)
-chempionat4 = Haydovchi("Qodir", 100)
-print(f"{chempionat1.getName()} --> {chempionat1.getFinishingTime()}")
-print(f"{chempionat2.getName()} --> {chempionat2.getFinishingTime()}")
-print(f"{chempionat3.getName()} --> {chempionat3.getFinishingTime()}")
-print(f"{chempionat4.getName()} --> {chempionat4.getFinishingTime()}")
-print(chempionat4._finish_time)
+print(chempionat.set_time(gp, driver1, tugash_vaqti1))
+print(chempionat.set_time(gp, driver2, tugash_vaqti2))
+print(chempionat.set_time(gp1, driver1, tugash_vaqti3))
+
+a = chempionat.getGrandPrix("katta halqa")
+print(a.get_gp_ranking())
+print(a.get_position(driver1))
+print(driver1.get_raced())
 
 # print(chempionat.defineGrandPrix('katta aylana yo\'li'))
 # print(chempionat.getGrandPrix('kattaaylana yo\'li'))
 
-#chempionat.createDriver('ibrohimjon')
-#chempionat.createDriver('ali')
-#print(chempionat.getDriver('ali'))
-#print(chempionat.getDrivers())
+# chempionat.createDriver('ibrohimjon')
+# chempionat.createDriver('ali')
+# print(chempionat.getDriver('ali'))
+# print(chempionat.getDrivers())
